85d1ebe0e4ef35f3020ff260d155d3e46dfa725a
Victor Rentea <victorrentea@gmail.com>
2019-09-10 12:55:44 +0300

    Am facut delegare + am facut o mare porcarie :din supe rclasa conteaza ce subclasa concreta exista de fapt

--- src/bscc/extendsToDelegation/ExtendsToDelegation.java
+++ src/bscc/extendsToDelegation/ExtendsToDelegation.java
@@ -5,29 +5,50 @@ public class ExtendsToDelegation {
 }
 
 
-abstract class Super {
-    protected void m() {
+
+//interface IImplem {
+//    void m();
+//}
+class Implem1 /*implements IImplem */{
+    public void m() {
         // implem1
     }
 }
+class Implem2 /*implements IImplem */{
+    public void m() {
+        // implem2
+    }
+}
+// ----------------------------
 
+abstract class Super {
+    public void metodaDinBaza() {
+        if (this instanceof A) {
+            A a = (A) this;
+
+        }
+    }
+}
 class A extends Super {
+
+    private final Implem1 implem = new Implem1();
+
     public void altaMetoda() {
-        m(); // apelez impl 1
+        implem.m(); // apelez impl 1
     }
 }
 class C extends Super {
+    private final Implem1 implem = new Implem1();
+
     public void altaMetoda() {
-        m(); // apelez impl 1
+        implem.m(); // apelez impl 1
     }
 }
 
 class B extends Super {
+    private final Implem2 implem = new Implem2();
+
     public void altaMetoda() {
-        m(); // implem 2 !! TZEAPA !
-    }
-    @Override
-    protected void m() {
-        // implem2
+        implem.m(); // implem 2 !! TZEAPA !
     }
 }
