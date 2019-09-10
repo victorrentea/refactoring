3378e0a0753b2dbabb9f66455add2eadbec9a17f
Victor Rentea <victorrentea@gmail.com>
2019-09-10 12:46:04 +0300

    super class hosting common logic

---
+++ src/bscc/extendsToDelegation/ExtendsToDelegation.java
@@ -0,0 +1,24 @@
+package bscc.extendsToDelegation;
+
+public class ExtendsToDelegation {
+
+}
+
+
+abstract class Super {
+    protected void m() {
+        // logica Specifica @@!#@!#!$!@@#!
+    }
+    protected void n() {
+        // logica 2asdsa dsa dsa dsa
+    }
+}
+
+class A extends Super {
+}
+
+class B extends Super {
+    public void altaMetoda() {
+        m();
+    }
+}
