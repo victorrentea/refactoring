20f6d48e91de77fe4b6914cb1928dd0dc61107e9
Victor Rentea <victorrentea@gmail.com>
2019-10-21 13:05:48 +0300

    plin de erori YOLO

---
+++ src/bscc/refactoringpatterns/AccumulationToCollecting.java
@@ -0,0 +1,93 @@
+package bscc.refactoringpatterns;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccumulationToCollecting {
+    public static void main(String[] args) {
+        QueryBuilder qb = new QueryBuilder();
+        StringBuilder sb = new StringBuilder();
+        Map<String, Object> map = new HashMap<>();
+
+        qb.buildSql(sb, map);
+        String sql = qb.getSql();
+    }
+}
+
+
+//@Service
+class Step1 {
+
+    public void process(ExecutionParameters execParams) {
+        execParams.setParam("date.pt.urmatorul.pas", 13L);
+    }
+}
+
+
+// @Service
+class QueryBuilder {
+
+    // @Autowired / @Inject
+
+//    private String sql = "SELECT * FROM KK WHERE 1=1 ";
+//    private Map<String, Object> params = new HashMap<>();
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void buildSql(/*criteria*/ StringBuilder sqlDePanaAcum, Map<String, Object> params) {
+        if (criteria.blabla) {
+            sqlDePanaAcum.append(" AND k.siPasta = ? ");
+            params.put("kiki",  criteria.kiki);
+        }
+    }
+    public void buildWhereForUser(/*criteria*/) {
+        if (criteria.blabla) {
+            sql += " AND u.siPasta = ? ";
+        }
+    }
+
+    public String addUserCriteria(userCriteria) {
+        if (userCritera.ceva) {
+            return " AND User...." ;
+        } else {
+            return "";
+        }
+    }
+
+
+
+    {
+        String sql = "";
+        sql += addUserCriteria(userCriteria);
+        sql = addUserCriteria(sql, userCriteria);
+        sql = addUserCriteria(sql, userCriteria);
+        sql = addUserCriteria(sql, userCriteria);
+        sql = addUserCriteria(sql, userCriteria);
+
+
+        BigDecimal ten = BigDecimal.TEN;
+        BigDecimal twenty = ten.add(ten);
+    }
+}
+
+class ImmutableBuilder {
+    private final String userpartName;
+    private final String userAddress;
+
+    ImmutableBuilder(String userpartName, String userAddress) {
+        this.userpartName = userpartName;
+        this.userAddress = userAddress;
+    }
+
+    public ImmutableBuilder withAddress(String address) {
+        return new ImmutableBuilder(userpartName, address);
+    }
+
+    public ChestieDeConstruit build() {
+        new ChestieDeConstruit
+    }
+}
+
---
+++ src/bscc/refactoringpatterns/CommandVsSwitch.java
@@ -0,0 +1,57 @@
+package bscc.refactoringpatterns;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandVsSwitch {
+    enum MessageType {
+        PLACE_ORDER(PlaceOrderCommand.class),
+        CANCEL_ORDER(CancelOrderCommand.class);
+
+        public final Class<? extends Command> commandClass;
+        MessageType(Class<? extends Command> commandClass) {
+            this.commandClass = commandClass;
+        }
+    }
+    private final static Map<String, Command> commandMap = new HashMap<>();
+
+    static {
+        commandMap.put("PLACE_ORDER", new PlaceOrderCommand());
+        commandMap.put("CANCEL_ORDER", new CancelOrderCommand());
+    }
+
+    public static void main(String[] args) {
+        String actionType = "PLACE_ORDER";
+//        switch (actionType) {
+//            case "PLACE_ORDER" : // logica multa 1
+//                break;
+//            case "CANCEL_ORDER" : // logica multa 2
+//                break;
+//        }
+        commandMap.get(actionType).execute();
+
+        commandMap.getOrDefault(actionType,
+                () -> {
+                    throw new IllegalArgumentException(actionType);
+                })
+                .execute();
+
+        MessageType messageType = MessageType.valueOf(actionType);
+
+        applicationContext.getBean(messageType.commandClass).execute();
+    }
+}
+interface Command {
+    void execute();
+}
+class PlaceOrderCommand implements Command {
+    public void execute () {
+        // logica multa 1
+    }
+}
+
+class CancelOrderCommand implements Command{
+    public void execute () {
+        // logica multa 2
+    }
+}
